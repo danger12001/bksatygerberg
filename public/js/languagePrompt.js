97bdcb736a9c1c7002137efe00fafdf1d52baec0
tallpaultheprogrammer <paul@reacttech.co>
2018-02-16 13:14:07 +0200

    frontend fro home page

--- public/js/languagePrompt.js
+++ public/js/languagePrompt.js
@@ -1,4 +1,44 @@
 var language = localStorage['language'];
+
+
+var translations = {
+  english: [
+    { id: 'title', text: 'MCSA Tygerberg Section' },
+    { id: 'nav-title', text: 'MCSA Tygerberg Section' },
+    { id: 'sections-dropdown', text: 'Sections' },
+    { id: 'loginBtn', text: 'Login'},
+    { id: 'logoutBtn', text: 'Logout'},
+    { id: 'join-us-btn', text: 'Join Us'},
+    { id: 'contact-us-btn', text: 'Contact Us' },
+    { id: 'events-btn', text: 'Events'},
+    { id: 'about-us-title', text: 'About Us'},
+    { id: 'about-us-text', text:
+        'The Tygerberg Section of the Mountain Club of South Africa (MCSA) was founded in 1979 and is one of the youngest of the 14 sections of the Mountain Club of South Africa. <br><br>The members are mostly from the northern suburbs of Cape Town.<br> <br>The Tygerberg section&#39;s activities are focused on hiking which varies from difficult wilderness hikes to family weekends with easy day hikes. <br> <br> The Mountain Club of South Africa was founded in 1891 and is one of only two mountain clubs in Africa which are affiliated to the world mountaineering body, the UIAA.'
+    },
+  ],
+  afrikaans: [
+    { id: 'title', text: 'BKSA Tygerberg Afdeling' },
+    { id: 'nav-title', text: 'BKSA Tygerberg Afdeling' },
+    { id: 'sections-dropdown', text: 'Afdelings' },
+    { id: 'loginBtn', text: 'Teken Aan'},
+    { id: 'logoutBtn', text: 'Teken Uit'},
+    { id: 'join-us-btn', text: 'Sluit Aan'},
+    { id: 'contact-us-btn', text: 'Kontak Ons' },
+    { id: 'events-btn', text: 'Aktiwiteite'},
+    { id: 'about-us-title', text: 'Oor ons'},
+    { id: 'about-us-text', text:
+        'Die Tygerberg Afdeling van die Bergklub van Suid Afrika (BKSA) is in 1979 gestig as een van die jongste van die 14 afdelings van die Bergklub van Suid Afrika. <br> <br> Die meeste lede is afkomstig van die noordelike voorstede van Kaapstad.<br> <br>Die Tygerberg Afdeling se aktiwiteite is hoofsaaklik ge-fokus op berg staptogte en wissel van\nmoeilike wildernis staptogte tot familie naweke met maklike dagstappe.<br><br>Die Bergklub van Suid Afrika is reeds in 1981 gestig en is een van slegs twee klubs in Afrika wat geaffilieer is aan die internasionale bergklim vereniging genaamd UIAA'
+    },
+  ]
+}
+
+
+var navElements = [{id: 'title'},{id: 'nav-title'}, {id: 'sections-dropdown'}, {id: 'loginBtn'}, {id: 'logoutBtn'}, {id: 'join-us-btn'}, {id: 'contact-us-btn'}, {id: 'events-btn'}, {id: 'about-us-title'}, {id: 'about-us-text'}];
+
+for(var x = 0; x< navElements.length; x++){
+  navElements[x].element = document.getElementById(navElements[x].id);
+}
+
 if(!language){
   swal({
      title: "Some Text" +
@@ -15,10 +55,44 @@ if(!language){
 }
 $(document).on('click', '.SwalBtn1', function() {
  //Some code 1
- localStorage['language'] = 'English';
+ localStorage['language'] = 'english';
+ window.location.reload()
 });
 $(document).on('click', '.SwalBtn2', function() {
  //Some code 2
- localStorage['language'] = 'Afrikaans';
+ localStorage['language'] = 'afrikaans';
+ window.location.reload()
 
 });
+
+  switch(language){
+    case 'english':
+      navElements.forEach(function(element){
+        translations[language].forEach(function(translation){
+          if(translation.id === element.id){
+            element.element.innerHTML = translation.text;
+          }
+        })
+      })
+    break;
+    case 'afrikaans':
+    navElements.forEach(function(element){
+      translations[language].forEach(function(translation){
+        if(translation.id === element.id){
+          console.log(translation.text)
+          element.element.innerHTML = translation.text;
+        }
+      })
+    })
+    break;
+    default:
+    language = 'english';
+    navElements.forEach(function(element){
+      translations[language].forEach(function(translation){
+        if(translation.id === element.id){
+          element.element.innerHTML = translation.text;
+        }
+      })
+    })
+
+  }
--- public/js/materialize/sideMenu.js
+++ public/js/materialize/sideMenu.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
    $('.collapsible').collapsible();
    $(".button-collapse").sideNav({ closeOnClick: true});
+    $('.carousel.carousel-slider').carousel({fullWidth: true, indicators: true});
+autoplay() 
+
+
 
  });
+  var urlmenu = document.getElementById( 'menu1' );
+  urlmenu.onchange = function() {
+       window.open(  this.options[ this.selectedIndex ].value );
+       this.selectedIndex = 0
+  };
+  function autoplay() {
+      $('.carousel').carousel('next');
+      setTimeout(autoplay, 4500);
+  }
